a6ac06cc71460ca2a3be0f8ead86ff5af5b20100
Szabolcs Csörgő <csorgosz@gmail.com>
2020-09-22 13:18:34 +0200

    Backend setup

---
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.szabidev.webshop_backend.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String getEnv(){
+        return "hello";
+    }
+}
