431f563c61b787930981d6357a90a75176ca134b
Szabolcs Csörgő <info@szabidev.com>
2020-10-28 09:21:39 +0100

    tab generalized

--- admin_frontend/src/component/dataTable/common/tabComponent.js
+++ admin_frontend/src/component/dataTable/common/tabComponent.js
@@ -4,6 +4,19 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import TabPanel from '../common/tabPanel'
 
+const tabs = [
+    {id: 'tab1', displayName: "Tab 1", index: 0, content: {
+        text: "i am tab 1"
+    }}, 
+    {id: 'tab2', displayName: "Tab 2", index: 1, content: {
+        text: "i am tab 2"
+    }},
+    {id: 'tab3', displayName: "Tab 3", index: 2,content: {
+        text: "i am tab 3"
+    }}
+]
+
+
 export default function TabComponent(props){
     const [value, setValue] = React.useState(0);
 
@@ -14,14 +27,13 @@ export default function TabComponent(props){
     return (
         <div>
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Item One"  />
-          <Tab label="Item Two" />
-          <Tab label="Item three"/>
+          {tabs.map( (tab) => (
+              <Tab label={tab.displayName}  />
+          ))}
         </Tabs>
-    
-        <TabPanel value={value} index={0} content="one"/>
-        <TabPanel value={value} index={1} content="two" />
-
+        {tabs.map( (tab) => (
+              <TabPanel value={value} index={tab.index} content={tab.content.text}/>
+          ))}
         </div>  
     )
 
