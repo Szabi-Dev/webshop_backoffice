9c6f83a591f7ee0b3cf21ec91bb73c26b5df3c68
Szabolcs Csörgő <info@szabidev.com>
2020-11-06 13:28:09 +0100

    java doc added

--- webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/CategoryFacade.java
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/CategoryFacade.java
@@ -2,7 +2,6 @@ package com.szabidev.webshop_backend.facade;
 
 import com.szabidev.webshop_backend.controller.dto.CategoryJson;
 import com.szabidev.webshop_backend.facade.dto.CategoryData;
-import com.szabidev.webshop_backend.facade.dto.PrivilegeData;
 import org.springframework.hateoas.CollectionModel;
 
 import java.util.Optional;
@@ -11,16 +10,46 @@ import java.util.Optional;
  * Facade to transform results for frontend
  */
 public interface CategoryFacade {
-
+    /**
+     * Method to fetch category by Id
+     * @param id - category id
+     * @return {@link Optional<CategoryData>}
+     */
     Optional<CategoryData> getCategoryById(Long id);
 
+    /**
+     * Method to create a category
+     * @param categoryJson - {@link CategoryJson}
+     * @return @link Optional<CategoryData>}
+     */
     Optional<CategoryData> createCategory(CategoryJson categoryJson);
 
+    /**
+     * Method to upodate category
+     * @param categoryJson - {@link CategoryJson}
+     * @param id - category id
+     * @return {@link Optional<CategoryData>}
+     */
     Optional<CategoryData> updateCategory(CategoryJson categoryJson, Long id);
 
+    /**
+     * Method to patch category
+     * @param categoryJson - {@link CategoryJson}
+     * @param id - category id
+     * @return {@link Optional<CategoryData>}
+     */
     Optional<CategoryData> patchCategory(CategoryJson categoryJson, Long id);
 
+    /**
+     * Method to delete category
+     * @param id - category id
+     * @return {@link Optional<CategoryData>}
+     */
     Optional<CategoryData> deleteCategory(Long id);
 
+    /**
+     * method to fetch all categories
+     * @return {@link CollectionModel<CategoryData>}
+     */
     CollectionModel<CategoryData> fetchAllCategories();
 }
--- webshop_backend/src/main/java/com/szabidev/webshop_backend/service/CategoryService.java
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/service/CategoryService.java
@@ -9,16 +9,47 @@ import java.util.Optional;
  * Service to provide category specific operation
  */
 public interface CategoryService {
-
+    /**
+     * Method to find all categories
+     *
+     * @return
+     */
     List<CategoryModel> findAllCategories();
 
+    /**
+     * Method to get Category by Id
+     * @param id - category id
+     * @return {@link CategoryModel}
+     */
     Optional<CategoryModel> getCategoryById(Long id);
 
+    /**
+     * Method to create a new Category
+     * @param categoryModel - {@link CategoryModel}
+     * @return {@link Optional<CategoryModel>}
+     */
     Optional<CategoryModel> createCategory(CategoryModel categoryModel);
 
+    /**
+     * Method to update category
+     * @param categoryModel - {@link CategoryModel}
+     * @param id - category id
+     * @return {@link Optional<CategoryModel>}
+     */
     Optional<CategoryModel> updateCategory(CategoryModel categoryModel, Long id);
 
+    /**
+     * Method to patch category (update just fields that are present)
+     * @param categoryModel - {@link CategoryModel}
+     * @param id - category id
+     * @return
+     */
     Optional<CategoryModel> patchCategory(CategoryModel categoryModel, Long id);
 
+    /**
+     * Method to delete category
+     * @param id - category id
+     * @return {@link Optional<CategoryModel>}
+     */
     Optional<CategoryModel>  deleteCategory(Long id);
 }
