373607f43ca1a254a703654aafab012f942c7ab7
Szabolcs Csörgő <info@szabidev.com>
2020-11-06 15:32:36 +0100

    javadoc added

--- webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/ProductFacade.java
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/ProductFacade.java
@@ -7,17 +7,46 @@ import org.springframework.hateoas.CollectionModel;
 import java.util.Optional;
 
 /**
- * facade to transform product data
+ * Facade to transform product data
  */
 public interface ProductFacade {
 
+    /**
+     * Method to fetch all products
+     *
+     * @return {@link CollectionModel<ProductData>}
+     */
     CollectionModel<ProductData> findAllProducts();
 
+    /**
+     * Method to get product by id
+     *
+     * @param id - product id
+     * @return {@link Optional<ProductData>}
+     */
     Optional<ProductData> getProductById(Long id);
 
+    /**
+     * Method to create a product
+     *
+     * @param productJson {@link ProductJson}
+     * @return {@link Optional<ProductData>}
+     */
     Optional<ProductData> createProduct(ProductJson productJson);
 
+    /**
+     * Method to patch product
+     *
+     * @param productJson {@link ProductJson}
+     * @param id product id
+     * @return {@link Optional<ProductData>}
+     */
     Optional<ProductData> patchProduct(ProductJson productJson, Long id);
 
+    /**
+     * Method to delete products
+     * @param id product id
+     * @return {@link Optional<ProductData>}
+     */
     Optional<ProductData> deleteProduct(Long id);
 }
--- webshop_backend/src/main/java/com/szabidev/webshop_backend/service/ProductService.java
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/service/ProductService.java
@@ -10,13 +10,39 @@ import java.util.Optional;
  */
 public interface ProductService {
 
+    /**
+     * Method to find all product
+     *
+     * @return {@link List<ProductModel>}
+     */
     List<ProductModel> findAllProducts();
 
+    /**
+     * Method to get product by id
+     * @param id product id
+     * @return {@link Optional<ProductModel>}
+     */
     Optional<ProductModel> getProductById(Long id);
 
+    /**
+     * Method to create a product
+     * @param productModel {@link ProductModel}
+     * @return {@link Optional<ProductModel>}
+     */
     Optional<ProductModel> createProduct(ProductModel productModel);
 
+    /**
+     * Method to patch product
+     * @param productModel {@link ProductModel}
+     * @param id product id
+     * @return {@link Optional<ProductModel>}
+     */
     Optional<ProductModel> patchProduct(ProductModel productModel, Long id);
 
+    /**
+     * Method to delete product
+     * @param id - product id
+     * @return {@link Optional<ProductModel>}
+     */
     Optional<ProductModel> deleteProduct(Long id);
 }
