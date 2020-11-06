b4122540b72834a41e0e999535a32daed5be11df
Szabolcs Csörgő <info@szabidev.com>
2020-11-06 15:27:30 +0100

    links to API added

--- webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/assembler/ProductDataAssembler.java
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/assembler/ProductDataAssembler.java
@@ -1,5 +1,8 @@
 package com.szabidev.webshop_backend.facade.assembler;
 
+import com.szabidev.webshop_backend.controller.CategoryController;
+import com.szabidev.webshop_backend.controller.ProductController;
+import com.szabidev.webshop_backend.facade.dto.CategoryData;
 import com.szabidev.webshop_backend.facade.dto.ProductData;
 import com.szabidev.webshop_backend.model.ProductModel;
 import com.szabidev.webshop_backend.service.LocalizationService;
@@ -11,6 +14,9 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component("productDataAssembler")
 public class ProductDataAssembler implements RepresentationModelAssembler<ProductModel,ProductData > {
 
@@ -18,12 +24,14 @@ public class ProductDataAssembler implements RepresentationModelAssembler<Produc
     private LocalizationService localizationService;
 
     private static final String ALL_PRODUCTS_REL = "products";
-    private static final String CREATE_CATEGORY_REL = "create";
+    private static final String CREATE_PRODUCT_REL = "create";
 
 
     @Override
     public ProductData toModel(ProductModel entity) {
         ProductData productData = convert(entity);
+        populateSelfLink(productData);
+        populateLinkToAll(productData, ALL_PRODUCTS_REL);
         return productData;
     }
 
@@ -31,7 +39,9 @@ public class ProductDataAssembler implements RepresentationModelAssembler<Produc
     public CollectionModel<ProductData> toCollectionModel(Iterable<? extends ProductModel> entities) {
         List<ProductData> productDataList = new ArrayList<>();
         entities.forEach(entity -> productDataList.add(toModel(entity)));
-        return CollectionModel.of(productDataList);
+        return CollectionModel.of(productDataList,
+                linkTo(methodOn(ProductController.class).findAllProducts()).withSelfRel(),
+                linkTo(methodOn(ProductController.class).createProduct(null)).withRel(CREATE_PRODUCT_REL));
     }
 
     private ProductData convert(ProductModel productModel) {
@@ -45,5 +55,13 @@ public class ProductDataAssembler implements RepresentationModelAssembler<Produc
         productData.setName(productModel.getLocalizations().get(lang).getName());
         productData.setDescription(productModel.getLocalizations().get(lang).getDescription());
         return productData;
+    }
+
+    private void populateSelfLink(ProductData productData){
+        productData.add(linkTo(methodOn(ProductController.class).getProductById(productData.getId())).withSelfRel());
+    }
+
+    private void populateLinkToAll(ProductData productData, String rel){
+        productData.add(linkTo(methodOn(ProductController.class).findAllProducts()).withRel(rel));
     }
 }
