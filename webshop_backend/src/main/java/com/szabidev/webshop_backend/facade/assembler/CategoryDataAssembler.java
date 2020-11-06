fef800c95d086591f2daad957234a38774abd663
Szabolcs Csörgő <info@szabidev.com>
2020-11-06 12:30:05 +0100

    links added to categoryData

--- webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/assembler/CategoryDataAssembler.java
+++ webshop_backend/src/main/java/com/szabidev/webshop_backend/facade/assembler/CategoryDataAssembler.java
@@ -1,6 +1,10 @@
 package com.szabidev.webshop_backend.facade.assembler;
 
+import com.szabidev.webshop_backend.controller.CategoryController;
+import com.szabidev.webshop_backend.controller.PrivilegeController;
+import com.szabidev.webshop_backend.controller.RoleController;
 import com.szabidev.webshop_backend.facade.dto.CategoryData;
+import com.szabidev.webshop_backend.facade.dto.RoleData;
 import com.szabidev.webshop_backend.model.CategoryModel;
 import com.szabidev.webshop_backend.service.LocalizationService;
 import org.springframework.hateoas.CollectionModel;
@@ -11,16 +15,23 @@ import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component("categoryDataAssembler")
 public class CategoryDataAssembler implements RepresentationModelAssembler<CategoryModel, CategoryData> {
 
     @Resource(name = "localizationService")
     private LocalizationService localizationService;
 
+    private static final String ALL_CATEGORIES_REL = "roles";
+    private static final String CREATE_CATEGORY_REL = "create";
 
     @Override
     public CategoryData toModel(CategoryModel entity) {
         CategoryData categoryData = convert(entity);
+        populateSelfLink(categoryData);
+        populateLinkToAll(categoryData, ALL_CATEGORIES_REL);
         return categoryData;
     }
 
@@ -28,7 +39,9 @@ public class CategoryDataAssembler implements RepresentationModelAssembler<Categ
     public CollectionModel<CategoryData> toCollectionModel(Iterable<? extends CategoryModel> entities) {
         List<CategoryData> categoryDataList = new ArrayList<>();
         entities.forEach(entity -> categoryDataList.add(this.convert(entity)));
-        return CollectionModel.of(categoryDataList);
+        return CollectionModel.of(categoryDataList,
+                linkTo(methodOn(CategoryController.class).fetchAllCategories()).withSelfRel(),
+                linkTo(methodOn(CategoryController.class).createCategory(null)).withRel(CREATE_CATEGORY_REL));
     }
 
     private CategoryData convert(CategoryModel categoryModel) {
@@ -38,4 +51,14 @@ public class CategoryDataAssembler implements RepresentationModelAssembler<Categ
         categoryData.setName(categoryModel.getLocalizations().get(lang).getCategoryName());
         return categoryData;
     }
+
+    private void populateSelfLink(CategoryData categoryData){
+        categoryData.add(linkTo(methodOn(CategoryController.class).getCategoryById(categoryData.getId())).withSelfRel());
+    }
+
+    private void populateLinkToAll(CategoryData categoryData, String rel){
+        categoryData.add(linkTo(methodOn(CategoryController.class).fetchAllCategories()).withRel(rel));
+    }
+
+
 }
