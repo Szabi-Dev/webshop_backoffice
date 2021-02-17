af7faa48ae4fe94a4309b595ebac7124dc772acb
Szabolcs Csörgő <info@szabidev.com>
2021-02-17 19:52:23 +0100

    basic layout

--- admin-frontend/src/app/app-routing.module.ts
+++ admin-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RolePageComponent} from './pages/role-page/role-page.component'
+import {UserPageComponent} from './pages/user-page/user-page.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'role', component: RolePageComponent},
+  {path: 'user', component: UserPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
